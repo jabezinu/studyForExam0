e25d28c3342590e47a2d81c6260050941c061b4d
jabezinu <jabezzinu31@gmail.com>
2025-05-08 01:13:12 +0300

    npm install jsdom

---
+++ script.js
@@ -0,0 +1,17 @@
+// const test(message) => {
+//     alert(message)
+// }
+
+// const getcontact(req, res) => {
+
+// }
+
+function test(message){
+    alert(message)
+}
+
+// document.getElementById("yab2").innerHTML = "text from js"
+// const yab = document.getElementById("yab2")
+// console.log(yab);
+const element = document.getElementById("yab2");
+console.log(element); // Will log the div element
